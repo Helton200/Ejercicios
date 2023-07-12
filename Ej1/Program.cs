20f114f4b355134228ef4a11a6b102bcedabe6f5
unknown <Helton@live.com.ar>
2023-07-11 22:23:27 -0300

    Commit Inicial

---
+++ Ej1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ej1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //El Ejercicio es una Calculadora
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PrimerPrograma/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PrimerPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Se imprime un mensaje en pantalla
+            Console.WriteLine("Hola Mundo!");
+        }
+    }
+}
