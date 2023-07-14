9a8482990932447433335da75d83ca910aac8c28
unknown <Helton@live.com.ar>
2023-07-13 23:28:35 -0300

    Calculadora Suma

--- 2.PrimerPrograma/Ej1/Program.cs
+++ 2.PrimerPrograma/Ej1/Program.cs
@@ -7,7 +7,32 @@ namespace Ej1
         static void Main(string[] args)
         {
             //El Ejercicio es una Calculadora
-            Console.WriteLine("Hello World!");
+            // Paso 0 Crear Variables
+            int n1;
+            int n2;
+            int resultado;
+
+            //Paso 1, Pedir Valores
+            Console
+                .WriteLine("Esta calculadora esta hecha solamente para sumar");
+            Console.WriteLine("Ingrese un Numero");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el Siguiente Numero");
+            n2 = int.Parse(Console.ReadLine());
+            Console
+                .WriteLine("El calculo de la suma de " +
+                n1 +
+                " y " +
+                n2 +
+                " es:");
+            Console.WriteLine("Cargando.");
+
+            resultado = n1 + n2;
+            Console.WriteLine("Cargando..");
+            Console.WriteLine("Cargando...");
+            Console.WriteLine("El resultado es: " + resultado);
+            //Paso 2, Ejecutar el Calculo
+            //Mostrar el Resultado
         }
     }
 }
